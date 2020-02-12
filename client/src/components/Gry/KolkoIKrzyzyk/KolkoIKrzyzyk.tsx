b99997aa0c0a5b50f8221e2692e8882fdd1a34cb
Krystian Kalita <krystian.kalita@zetosoftware.pl>
2020-02-12 14:31:38 +0100

    kik - narysowana plansza i pionki, ułożony layout

--- client/src/components/Gry/KolkoIKrzyzyk/KolkoIKrzyzyk.tsx
+++ client/src/components/Gry/KolkoIKrzyzyk/KolkoIKrzyzyk.tsx
@@ -4,6 +4,24 @@ import socketIOClient from "socket.io-client";
 import "./KolkoIKrzyzyk.css";
 
 const KolkoIKrzyzyk = () => {
+  const plansza = <svg viewBox="0 0 300 300">
+  <g stroke="#000" strokeWidth="2">
+    <path d="m100 0v300" fill="none" />
+    <path d="m200 0v300" fill="none" />
+    <path d="m0 100h300" fill="none" />
+    <path d="m0 200h300" fill="none" />
+  </g>
+</svg>;
+
+const cross =     <svg viewBox="0 0 100 100">
+  <line x1="0" y1="0" x2="100" y2="100" stroke="#000" />
+  <line x1="0" y1="100" x2="100" y2="0" stroke="#000" />
+</svg>;
+
+const circle = <svg viewBox="0 0 100 100">
+  <circle cx="50" cy="50" r="49" stroke="#000" fill="none" />
+</svg>;
+
   const endpoint = "http://localhost:3001/users";
 
   // useEffect(() => {
@@ -19,7 +37,7 @@ const KolkoIKrzyzyk = () => {
   // }, []);
 
   return <div className="KolkoIKrzyzyk">
-    <svg style={{background: "red"}}></svg>
+    {plansza}
   </div>;
 };
 
